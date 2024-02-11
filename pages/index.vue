0b969196817968848fab7edd8250378f7ef7d70f
BuildTools <unconfigured@null.spigotmc.org>
2024-02-11 23:19:28 +0900

    AutoPost

---
+++ pages/index.vue
@@ -0,0 +1,106 @@
+<template>
+    <div>
+        <div class="input_field">
+        <h1>同時投稿くん</h1>
+        <label>
+            <p>Bluesky ID</p>
+            <input type="text" v-model="bs_id">
+            <p>Bluesky Password</p>
+            <input type="password" v-model="bs_pw">
+            <p>投稿する内容</p>
+            <textarea name="post_content" cols="25" rows="12" v-model="ct"></textarea>
+            <p>Blueskyは自動で投稿するから最終確認して</p>
+            <button @click="btClicked">投稿する</button>
+        </label>
+        <p></p><a href="https://twitter.com/tttaaayou">作者のXを見る</a>
+        </div>
+    </div>
+</template>
+
+<script>
+import {BskyAgent} from "@atproto/api"
+
+    export default {
+        data () {
+            return {
+                bs_id: '',
+                bs_pw: '',
+                ct: '',
+            }
+        },
+        methods: {
+            async btClicked () {
+
+                if(!this.bs_id || !this.bs_pw || !this.ct) {
+                    console.log("入力されていない値があるよ")
+                }
+                else {
+                    console.log("bs_id: " + this.bs_id)
+                    console.log("bs_pw: " + this.bs_pw)
+                    console.log("content: " + this.ct)
+
+                    const agent = new BskyAgent({ service: "https://bsky.social"});
+
+                    await agent.login({
+                        identifier: this.bs_id,
+                        password: this.bs_pw,
+                    })
+
+                    await agent.post({
+                        text: this.ct,
+                        langs: ["ja"]
+                    })
+
+                    window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent(this.ct))
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+    html {
+        font: 400 16px -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-font-smoothing: antialiased;
+        box-sizing: border-box;
+    }
+
+    a {
+        margin: 2px;
+        padding: 6px;
+        color: #4169e1;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #f0f8ff;
+        background-color: #4169e1;
+        border-radius: 4px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    .input_field {
+        padding: 10px;
+        margin: 0 auto;
+        width: 700px;
+        height: auto;
+        background-color: #f5f5f5;
+    }
+    input {
+        font-size: 20px;
+    }
+    textarea {
+        resize: none;
+        font-size: 20px;
+    }
+    button {
+        width: 500px;
+        height: 50px;
+    }
+</style>
